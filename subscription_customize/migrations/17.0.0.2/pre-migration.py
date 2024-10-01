f2b389e50ea637180d3ff29f4007704272802624
Christian Radeschnig <cra@solute.de>
2024-10-01 15:05:09 +0200

    pre-scripts

--- subscription_customize/migrations/17.0.0.2/pre-migration.py
+++ subscription_customize/migrations/17.0.0.2/pre-migration.py
@@ -4,21 +4,21 @@ from odoo.upgrade import util
 def migrate(cr, version):
 
     cr.execute("UPDATE sale_order "
-               "SET sale_order.amount_at_start = sale_subscription.amount_at_start"
-               "FROM sale_subcription"
+               "SET sale_order.amount_at_start = sale_subscription.amount_at_start "
+               "FROM sale_subscription "
                "WHERE sale_order.id = sale_subscription.new_sale_order_id;")
 
     cr.execute("UPDATE sale_order "
-              "SET sale_order.parent_id = sale_subscription.parent_id"
-              "FROM sale_subcription "
+              "SET sale_order.parent_id = sale_subscription.parent_id "
+              "FROM sale_subscription "
               "WHERE sale_order.id = sale_subscription.new_sale_order_id;")
 
     cr.execute("UPDATE sale_order "
               "SET sale_order.child_ids = sale_subscription.child_ids "
-              "FROM sale_subcription"
+              "FROM sale_subscription "
               "WHERE sale_order.id = sale_subscription.new_sale_order_id;")
 
     cr.execute("UPDATE sale_order "
-              "SET sale_order.child_recurring_total = sale_subscription.child_recurring_total"
-              "FROM sale_subcription"
+              "SET sale_order.child_recurring_total = sale_subscription.child_recurring_total "
+              "FROM sale_subscription "
               "WHERE sale_order.id = sale_subscription.new_sale_order_id;")
